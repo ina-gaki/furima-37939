7377d7bf08a55b427bc5564b81a9f480249b0db8
ina-gaki <ina.p1192@gmail.com>
2022-05-20 15:13:15 +0900

    Rubocopを実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,25 +54,25 @@ RSpec.describe User, type: :model do
       user = User.new(nickname: 'test', email: 'test@gmail.com', password: '00000000', password_confirmation: '00000000',
                       family_name: '仮', first_name: 'ﾀﾛｳ', family_name_kana: 'カリ', first_name_kana: 'タロウ', birthday: "'2000/01/01'")
       user.valid?
-      expect(user.errors.full_messages).to include("First name is invalid")
+      expect(user.errors.full_messages).to include('First name is invalid')
     end
     it 'family_nameが全角（漢字・ひらがな・カタカナ）以外では登録できない' do
       user = User.new(nickname: 'test', email: 'test@gmail.com', password: '00000000', password_confirmation: '00000000',
                       family_name: 'ｶﾘ', first_name: '太郎', family_name_kana: 'カリ', first_name_kana: 'タロウ', birthday: "'2000/01/01'")
       user.valid?
-      expect(user.errors.full_messages).to include("Family name is invalid")
+      expect(user.errors.full_messages).to include('Family name is invalid')
     end
     it 'first_name_kanaが全角（カタカナ）以外では登録できない' do
       user = User.new(nickname: 'test', email: 'test@gmail.com', password: '00000000', password_confirmation: '00000000',
                       family_name: '仮', first_name: '太郎', family_name_kana: 'カリ', first_name_kana: '太郎', birthday: "'2000/01/01'")
       user.valid?
-      expect(user.errors.full_messages).to include("First name kana is invalid")
+      expect(user.errors.full_messages).to include('First name kana is invalid')
     end
     it 'family_name_kanaが全角（カタカナ）以外では登録できない' do
       user = User.new(nickname: 'test', email: 'test@gmail.com', password: '00000000', password_confirmation: '00000000',
                       family_name: '仮', first_name: '太郎', family_name_kana: '仮', first_name_kana: 'タロウ', birthday: "'2000/01/01'")
       user.valid?
-      expect(user.errors.full_messages).to include("Family name kana is invalid")
+      expect(user.errors.full_messages).to include('Family name kana is invalid')
     end
   end
 end
