0545eb14d8f6dc08c65c1a59a09960093550a2b6
ina-gaki <ina.p1192@gmail.com>
2022-05-20 19:10:51 +0900

    date型の表記へ変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,6 +8,6 @@ FactoryBot.define do
     first_name            {'太郎'}
     family_name_kana      {'カリ'}
     first_name_kana       {'タロウ'}
-    birthday              {"'2000/01/01'"}
+    birthday              {"1983-07-22"}
   end
 end
