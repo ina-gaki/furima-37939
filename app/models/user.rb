7b95a68f6ecae32b249a502d9cde4f5dd75cbed2
ina-gaki <ina.p1192@gmail.com>
2022-05-20 14:45:35 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   validates :nickname,            presence: true
   validates :email,               presence: true
   validates :encrypted_password,  presence: true
-  validates :first_name,          presence: true
-  validates :family_name,         presence: true
-  validates :first_name_kana,     presence: true
-  validates :family_name_kana,    presence: true
+  validates :first_name,          presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name,         presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name_kana,     presence: true,format: { with: /\A[ァ-ヴー]+\z/u}
+  validates :family_name_kana,    presence: true,format: { with: /\A[ァ-ヴー]+\z/u}
   validates :birthday,            presence: true
 end
