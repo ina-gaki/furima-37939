15aaf5e9241962a7b8da6a39c31dc0e2d848e1d5
ina-gaki <ina.p1192@gmail.com>
2022-05-23 12:35:07 +0900

    保存機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,20 @@ class ItemsController < ApplicationController
 
   def new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:description,:category_id,:item_status_id,:cost_id,:prefecture_id,:send_days_id,:price)
+  end
+
 end
