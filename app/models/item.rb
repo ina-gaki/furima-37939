099f19aa312f20d3fc5ce1a9a37db0324e4ca64e
ina-gaki <ina.p1192@gmail.com>
2022-05-23 12:34:51 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,11 @@ class Item < ApplicationRecord
   validates :image,          presence: true
   validates :name,           presence: true
   validates :description,    presence: true
-  validates :category_id,    presence: true , numericality: { other_than: 1 } 
-  validates :item_status_id, presence: true , numericality: { other_than: 1 } 
-  validates :cost_id,        presence: true , numericality: { other_than: 1 } 
-  validates :prefecture_id,  presence: true , numericality: { other_than: 1 } 
-  validates :send_days_id,   presence: true , numericality: { other_than: 1 } 
+  validates :category_id,    presence: true , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :item_status_id, presence: true , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :cost_id,        presence: true , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id,  presence: true , numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :send_days_id,   presence: true , numericality: { other_than: 1 , message: "can't be blank"} 
   validates :price,          presence: true
   validates :user,           presence: true
 end
