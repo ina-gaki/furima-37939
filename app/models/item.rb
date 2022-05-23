1a9fa6df87ca1571c2b952025a716007b179f8a3
ina-gaki <ina.p1192@gmail.com>
2022-05-23 10:42:24 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,11 @@ class Item < ApplicationRecord
   validates :image,          presence: true
   validates :name,           presence: true
   validates :description,    presence: true
-  validates :category_id,    presence: true
-  validates :item_status_id, presence: true
-  validates :cost_id,        presence: true
-  validates :prefecture_id,  presence: true
-  validates :send_days_id,   presence: true
+  validates :category_id,    presence: true , numericality: { other_than: 1 } 
+  validates :item_status_id, presence: true , numericality: { other_than: 1 } 
+  validates :cost_id,        presence: true , numericality: { other_than: 1 } 
+  validates :prefecture_id,  presence: true , numericality: { other_than: 1 } 
+  validates :send_days_id,   presence: true , numericality: { other_than: 1 } 
   validates :price,          presence: true
   validates :user,           presence: true
 end
