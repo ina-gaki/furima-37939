29a2f4a43a07ce591eaf2574a3a1a8ad953d0506
ina-gaki <ina.p1192@gmail.com>
2022-05-23 10:31:22 +0900

    itemsモデルに紐づく各モデルファイル生成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :cost
+  belongs_to :prefecture
+  belongs_to :send_days
   has_one_attached :image
 end
