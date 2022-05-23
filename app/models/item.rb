c84b5d683c487af5bb69144003a22e15131a8652
ina-gaki <ina.p1192@gmail.com>
2022-05-23 10:39:18 +0900

    バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,15 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :send_days
   has_one_attached :image
+
+  validates :image,          presence: true
+  validates :name,           presence: true
+  validates :description,    presence: true
+  validates :category_id,    presence: true
+  validates :item_status_id, presence: true
+  validates :cost_id,        presence: true
+  validates :prefecture_id,  presence: true
+  validates :send_days_id,   presence: true
+  validates :price,          presence: true
+  validates :user,           presence: true
 end
