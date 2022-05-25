b86e58b0899bcf4747327fca348f00dfc2a714df
ina-gaki <ina.p1192@gmail.com>
2022-05-25 14:15:32 +0900

    destroyアクション記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user! , only: [:new ,:edit]
-  before_action :set_item, only: [:edit ,:show ,:update]
+  before_action :set_item, only: [:edit ,:show ,:update ,:destroy]
   before_action :move_to_index, only: :edit
 
 
@@ -33,6 +33,10 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    item.destroy
   end
 
   private
