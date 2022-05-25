c1dbeb44f1db6316c2397c34f064227d52bb275f
ina-gaki <ina.p1192@gmail.com>
2022-05-26 06:49:48 +0900

    アクション追加

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,12 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, except: [:index ,:new ,:create]
+
   def index
+  end
+
+  def new
+  end
+
+  def create
   end
 end
