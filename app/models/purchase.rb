6959764b3e5b180bb001fe4b8fb69316a445b7f1
ina-gaki <ina.p1192@gmail.com>
2022-05-26 10:28:51 +0900

    バリデーション記述

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,4 +2,8 @@ class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :item
   belongs_to :shipping_address
+
+
+  validates :item,     presence: true 
+  validates :user,     presence: true 
 end
