7968ffd6d6adeaf1830a84f98fa96a2fbad0f804
ina-gaki <ina.p1192@gmail.com>
2022-05-26 09:23:48 +0900

    createアクション記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,5 +8,18 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @purchase = Purchase.create(purchase_params)
+    ShippingAddress.create(address_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def purchase_params
+    params.merge(item_id: @item.id ,user_id: current_user.id)
+  end
+
+  def address_params
+    params.permit(:postal_code, :prefecture_id, :city, :shipping_address, :building, :phone_number, :purchase_id)
   end
 end
