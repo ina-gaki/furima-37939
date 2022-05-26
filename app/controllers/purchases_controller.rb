0183df9217dd49ca47458c7769f07c8a0ff9a49a
ina-gaki <ina.p1192@gmail.com>
2022-05-26 11:55:42 +0900

    formオブジェクト生成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,21 +5,23 @@ class PurchasesController < ApplicationController
   end
 
   def new
+    @total_purchase = TotalPurchase.new
   end
 
   def create
-    @purchase = Purchase.create(purchase_params)
-    ShippingAddress.create(address_params)
-    redirect_to root_path
+    @total_purchase = TotalPurchase.new(purchase_params)
+    if @total_purchase.valid?
+      @total_purchase.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def purchase_params
-    params.merge(item_id: @item.id ,user_id: current_user.id)
+    params.require(:total_purchase).permit(:postal_code, :prefecture_id, :city, :shipping_address, :building, :phone_number, :purchase_id).merge(item_id: @item.id ,user_id: current_user.id)
   end
 
-  def address_params
-    params.permit(:postal_code, :prefecture_id, :city, :shipping_address, :building, :phone_number, :purchase_id)
-  end
 end
--- app/models/total_purchase.rb
+++ app/models/total_purchase.rb
@@ -2,12 +2,17 @@ class TotalPurchase
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :shipping_address, :building, :phone_number, :purchase, :item_id, :user_id
 
-  validates :item_id,                    presence: true 
-  validates :user_id,                    presence: true 
+  validates :item_id,                 presence: true 
+  validates :user_id,                 presence: true 
   validates :postal_code,             presence: true , format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture_id,           presence: true , numericality: { other_than: 1 , message: "can't be blank"} 
   validates :city,                    presence: true 
   validates :shipping_address,        presence: true 
   validates :phone_number,            presence: true 
   validates :purchase,                presence: true 
+
+  def save
+    totalpurshase = TotalPurchase.create(item_id: item.id ,user_id: current_user.id)
+    ShippingAddress.create(postal_code: postal_code, prefecture: prefecture, city: city, shipping_address: shipping_address, building: building , phone_number: phone_number, purchase_id: purchase.id)
+  end
 end
