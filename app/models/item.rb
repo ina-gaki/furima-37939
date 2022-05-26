85084213ee3951947996041123cfc80f074a5919
ina-gaki <ina.p1192@gmail.com>
2022-05-26 10:41:54 +0900

    アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :send_days
   has_one_attached :image
   belongs_to :user
-  belongs_to :purchase
+  had_one    :purchase
 
   validates :image,          presence: true
   validates :name,           presence: true , length: { maximum: 40 }
