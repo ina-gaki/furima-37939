e66045905ca49b47eec2277def3073e37c3fc8e9
ina-gaki <ina.p1192@gmail.com>
2022-05-28 14:17:00 +0900

    条件式修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -54,7 +54,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless current_user.id == @item.user_id
+    if @item.purchase.present? ||current_user.id != @item.user_id
       redirect_to action: :index
     end
   end
