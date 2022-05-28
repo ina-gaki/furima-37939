f07e1b3853705f95cb7497a23b41c86a908df9a8
ina-gaki <ina.p1192@gmail.com>
2022-05-28 16:09:52 +0900

    バリデーションのリファレンス

--- app/models/total_purchase.rb
+++ app/models/total_purchase.rb
@@ -2,14 +2,16 @@ class TotalPurchase
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :shipping_address, :building, :phone_number, :purchase, :item_id, :user_id, :token
 
-  validates :item_id,                 presence: true 
-  validates :user_id,                 presence: true 
-  validates :postal_code,             presence: true , format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :prefecture_id,           presence: true , numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :city,                    presence: true 
-  validates :shipping_address,        presence: true 
-  validates :phone_number,            presence: true , format: {with: /\A\d{10}$|^\d{11}\z/ }
-  validates :token, presence: true
+  with_options presence: true do
+    validates :item_id                 
+    validates :user_id                 
+    validates :postal_code , format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id , numericality: { other_than: 1 , message: "can't be blank"} 
+    validates :city
+    validates :shipping_address
+    validates :phone_number, format: {with: /\A\d{10}$|^\d{11}\z/ }
+    validates :token
+  end
 
   def save
     purchase = Purchase.create(item_id: item_id ,user_id: user_id)
