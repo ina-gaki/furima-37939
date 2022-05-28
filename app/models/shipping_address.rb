ebbb140e63bb44306fbe35badecc56757244f6cf
ina-gaki <ina.p1192@gmail.com>
2022-05-28 15:53:12 +0900

    不要なバリデーション削除

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,11 +1,3 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :purchase
-
-
-  validates :postal_code,             presence: true , format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :prefecture_id,           presence: true , numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :city,                    presence: true 
-  validates :shipping_address,        presence: true 
-  validates :phone_number,            presence: true 
-  validates :purchase,                presence: true 
 end
